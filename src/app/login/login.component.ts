e5f811ef56886696ff7f0f04b11b9ca0a954ec5a
rachitshrivastava236 <32490729+rachitshrivastava236@users.noreply.github.com>
2019-08-21 11:29:20 +0530

    Login Module added with styling attributes, Login Module in progress

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+
+loginForm:FormGroup;
 
   ngOnInit() {
+    this.loginForm=this.fb.group(
+      {email:['',[Validators.required,Validators.email]],
+      pass :['',[Validators.required]]}
+    )
+  }
+
+  loginSubmit(){
+    console.log("Success");
   }
 
 }
